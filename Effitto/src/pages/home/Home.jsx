23e3e1294edec06a5b3ee6d2a054ea8bc532a8dd
Vraj1103 <svraj073@gmail.com>
2023-04-08 21:54:53 +0530

    added footer and project section with project carousal

--- Effitto/src/pages/home/Home.jsx
+++ Effitto/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Home.scss";
 import CatCard from '../../components/catCard/CatCard';
+// import ProjectCard from "../../components/projectCard/ProjectCard";
 import {cards} from '../../data'
+import {projects} from '../../data'
 import Featured from "../../components/featured/Featured";
 import Slide from "../../components/Slide/Slide";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 const Home = () =>{
     return(
         <div className="home">
@@ -55,7 +58,7 @@ const Home = () =>{
             <div className="features dark">
                     <div className="container">
                         <div className="item">
-                            <h1>Effitto Business</h1>
+                            <h1><span className="effitto">Effitto</span>Business</h1>
                             <h1>A business solution desinged for students</h1>
                             <p>Upgrade to a curated experience packed with tools and benefits,
                             dedicated to businesses</p>
@@ -75,13 +78,18 @@ const Home = () =>{
                                 <img src="./img/check.png" alt="check mark"/>
                                 Connect to freelaÅcers with proven business experience
                             </div>
-                            <button>Explore Effitto Business</button>
+                            <button>Explore <span className="effitto_button">Effitto</span> Business</button>
                         </div>
                         <div className="item">
                         <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png" alt="feature"/>
                         </div>
                     </div>
             </div>
+            <Slide slidesToShow={4} arrowsScroll={4}>
+                {projects.map(card=>(
+                    <ProjectCard key={card.id} item={card}/>
+                ))}
+            </Slide>
         </div>
     )
 }
